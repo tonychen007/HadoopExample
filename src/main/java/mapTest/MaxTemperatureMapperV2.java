653d966eb429a617822b329a7e2466e7a5746de7
tony.chen <tony.chen@non.com>
2019-05-31 19:51:35 +0800

    add mapper reducer test

---
+++ src/main/java/mapTest/MaxTemperatureMapperV2.java
@@ -0,0 +1,20 @@
+package mapTest;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class MaxTemperatureMapperV2 extends Mapper<LongWritable, Text, Text, IntWritable> {
+    private NcdcRecordParser parser = new NcdcRecordParser();
+
+    @Override
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        parser.parse(value);
+        if (parser.isValidTemperature()) {
+            context.write(new Text(parser.getYear()), new IntWritable(parser.getAirTemperature()));
+        }
+    }
+}
